0f9cd134e21c8f94682f9400c6e397edb14c9a4f
Justin Chan <j948698106@gmail.com>
2025-11-14 02:19:19 -0700

    Table will now shown in sorted order and will react to changes

--- lib/pages/review_sync.dart
+++ lib/pages/review_sync.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:open_split_time_v2/widgets/dropdown_menu.dart';
 import 'package:open_split_time_v2/widgets/page_router.dart';
 import 'package:open_split_time_v2/widgets/review_sync_widgets/sync_export_footer.dart';
+import 'package:open_split_time_v2/widgets/review_sync_widgets/review_sync_data_table.dart';
 import 'dart:developer' as developer;
 
 class ReviewSyncPage extends StatefulWidget {
@@ -77,60 +78,7 @@ class _ReviewSyncPageState extends State<ReviewSyncPage> {
                 ],
               ),
               const SizedBox(height: 10),
-              Expanded(
-                child: SingleChildScrollView(
-                    scrollDirection: Axis.vertical,
-                    child: Table(
-                      columnWidths: const {
-                        0: FlexColumnWidth(1), // Bib #
-                        1: FlexColumnWidth(3), // Name
-                        2: FlexColumnWidth(1), // In/Out
-                        3: FlexColumnWidth(2), // Time
-                      },
-                      border: const TableBorder(
-                        top: BorderSide(width: 0.5, color: Colors.grey),
-                        bottom: BorderSide(width: 0.5, color: Colors.grey),
-                        horizontalInside:
-                            BorderSide(width: 0.5, color: Colors.grey),
-                      ),
-                      children: [
-                        TableRow(
-                          children: const [
-                            Padding(
-                                padding: EdgeInsets.all(8), child: Text('123')),
-                            Padding(
-                                padding: EdgeInsets.all(8),
-                                child: Text('Sarah')),
-                            Padding(
-                                padding: EdgeInsets.all(8), child: Text('In')),
-                            Padding(
-                                padding: EdgeInsets.all(8),
-                                child: Align(
-                                  alignment: Alignment.centerRight,
-                                  child: Text('17:30:12'),
-                                )),
-                          ],
-                        ),
-                        TableRow(
-                          children: const [
-                            Padding(
-                                padding: EdgeInsets.all(8), child: Text('456')),
-                            Padding(
-                                padding: EdgeInsets.all(8),
-                                child: Text('Janine')),
-                            Padding(
-                                padding: EdgeInsets.all(8), child: Text('Out')),
-                            Padding(
-                                padding: EdgeInsets.all(8),
-                                child: Align(
-                                  alignment: Alignment.centerRight,
-                                  child: Text('12:30:45'),
-                                )),
-                          ],
-                        ),
-                      ],
-                    )),
-              ),
+              ReviewSyncDataTable(sortBy: sortBy!),
             ],
           ),
         ),
